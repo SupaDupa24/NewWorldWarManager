bf3213a815c156faf733b71876bc6ce8faffd5f4
Matthew Martin <zhintateo19924559@gmail.com>
2021-10-19 13:12:33 -0500

    Created server.js for router connections

---
+++ NewWorldWarManager-BE/api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.status(200).json({
+		mes:"Hello, the server is running"
+	});
+});
+
+server.use((err, req, res, next) => {
+	err.statusCode = err.statusCode ? err.statusCode : 500;
+	res.status(err.statusCode).json({
+		mes: err.message,
+		stack: err.stack
+	});
+});
+
+module.exports = server;
