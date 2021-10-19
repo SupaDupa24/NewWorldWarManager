0bfb04213247b36ea8fed175a693e1cb36bac8aa
Joseph Little <josephalittl92@gmail.com>
2021-10-19 13:22:39 -0500

    Added Login.js

---
+++ warmanager/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            
+        );
+    }
+}
