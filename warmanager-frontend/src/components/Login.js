59179a60fa53bc3e293644cf1e4e50afefc7d18f
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2021-10-21 10:56:10 -0400

    adding some more styles

--- warmanager-frontend/src/components/Login.js
+++ warmanager-frontend/src/components/Login.js
@@ -5,10 +5,10 @@ const Login = props => {
         <>
 
             <h1>Welcome to New World War Manager!</h1>
-            <div className style={{ backgroundColor: "grey" }, { padding: "5%" }}>
+            <div className style={{ background: "#9DA9A3" }, { padding: "5%" }}>
                 <form>
-                    <input type="text" placeholder="Username"></input><br />
-                    <input type="password" placeholder="Password"></input><br />
+                    <input style={{margin: "1%"}} type="text" placeholder="Username"></input><br />
+                    <input style={{margin: "1%"}} type="password" placeholder="Password"></input><br />
                     <button type="submit">Log In</button>
                 </form>
             </div>
