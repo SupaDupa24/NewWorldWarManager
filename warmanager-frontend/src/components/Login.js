da97450fa525cdaad25e2ab427831e6ec08d5c67
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2021-10-21 10:51:21 -0400

    trrying inline styling on the login page

--- warmanager-frontend/src/components/Login.js
+++ warmanager-frontend/src/components/Login.js
@@ -3,10 +3,15 @@ import React from 'react';
 const Login = props => {
     return (
         <>
+
             <h1>Welcome to New World War Manager!</h1>
-            <input className="usrLogin" type="text" placeholder="Username"></input><br />
-            <input className="usrLogin" type="password" placeholder="Password"></input><br />
-            <button type="submit">Log In</button>
+            <div className style={{ backgroundColor: "grey" }, { padding: "5%" }}>
+                <form>
+                    <input type="text" placeholder="Username"></input><br />
+                    <input type="password" placeholder="Password"></input><br />
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
         </>
     );
 }
