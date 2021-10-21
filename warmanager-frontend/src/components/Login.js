c7f1104392815e8e7dd23b0948a1579a22a67539
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2021-10-21 09:42:04 -0400

    added an ID tag to the login input fields to apply css attributes to them

--- warmanager-frontend/src/components/Login.js
+++ warmanager-frontend/src/components/Login.js
@@ -4,8 +4,8 @@ const Login = props => {
     return (
         <>
             <h1>Welcome to New World War Manager!</h1>
-            <input type="text" placeholder="Username"></input><br />
-            <input type="password" placeholder="Password"></input><br />
+            <input id="usrLogin" type="text" placeholder="Username"></input><br />
+            <input id="usrLogin" type="password" placeholder="Password"></input><br />
             <button type="submit">Log In</button>
         </>
     );
